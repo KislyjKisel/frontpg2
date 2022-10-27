4d8f833be1bc60fa5295629edc48cc7e43b84ca2
Daniil Kisel <danilakisel@gmail.com>
2022-10-27 19:21:43 +0300

    hi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const h = () => alert("text");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder='login' type='text'></input>
+      <input placeholder='password' type='password'></input>
+      <button onClick={() => alert({ "smth": "here" })}>submit</button>
     </div>
   );
 }
