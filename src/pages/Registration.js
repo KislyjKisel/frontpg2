f4b88920c3f4201792e1abce51619f394f707143
Daniil Kisel <danilakisel@gmail.com>
2022-11-29 17:44:22 +0300

    Use MUI for registration page

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,5 +1,7 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate, Link as RLink } from "react-router-dom";
 import { useState } from 'react';
+import { Box, Button, Container, Link, TextField, Typography } from '@mui/material';
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 
 import request from '../request';
 
@@ -43,42 +45,97 @@ export function Registration(props) {
     };
 
     return (
-        <div>
-            <h2>Registration</h2>
-            <Link to="/login">To Login</Link>
-            <form onSubmit={handleSubmit}>
-                <input
-                    name="login"
-                    type="text"
-                    placeholder="Login"
-                    value={state.login}
-                    onChange={handleChange}
-                />
-                <input
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    value={state.password}
-                    onChange={handleChange}
-                />
-                <input
-                    name="firstName"
-                    type="text"
-                    placeholder="First Name"
-                    value={state.firstName}
-                    onChange={handleChange}
-                />
-                <input
-                    name="lastName"
-                    type="text"
-                    placeholder="Last Name"
-                    value={state.lastName}
-                    onChange={handleChange}
-                />
-                <button type="submit">
-                    Submit
-                </button>
-            </form>
-        </div>
+        <Container maxWidth="sm" sx={{ alignItems: "center" }}>
+            <Box
+                sx={{
+                    mt: 10,
+                    ml: "auto",
+                    mr: "auto",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    width: "75%",
+                }}
+            >
+                <Typography component="h1" variant="h5">
+                    Register
+                </Typography>
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit}
+                    sx={{
+                        mt: 5,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        gap: 2,
+                        width: "100%",
+                    }}
+                >
+                    <TextField
+                        variant="outlined"
+                        required
+                        fullWidth
+                        name="login"
+                        label="Login"
+                        value={state.login}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        variant="outlined"
+                        required
+                        fullWidth
+                        name="password"
+                        type="password"
+                        label="Password"
+                        value={state.password}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        variant="outlined"
+                        required
+                        fullWidth
+                        name="firstName"
+                        label="First Name"
+                        value={state.firstName}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        variant="outlined"
+                        required
+                        fullWidth
+                        name="lastName"
+                        label="Last Name"
+                        value={state.lastName}
+                        onChange={handleChange}
+                    />
+                    <Button
+                        variant="contained"
+                        fullWidth
+                        type="submit"
+                    >
+                        Register
+                    </Button>
+                </Box>
+                <Grid2
+                    container
+                    spacing={0}
+                    sx={{
+                        mt: 1,
+                        width: "100%",
+                    }}
+                >
+                    <Grid2 item xsOffset={6} xs>
+                        <Link
+                            component={RLink}
+                            to="/login"
+                            variant="body2"
+                        >
+                            Already have an account? Login
+                        </Link>
+                    </Grid2>
+                </Grid2>
+            </Box>
+        </Container>
     );
 }
